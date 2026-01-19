cee5660a5af71888b8c9619829107214cc0604bb
Sathsarani Kangara <prabodhiyakangara@gmail.com>
2026-01-19 23:49:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import csv
 
+students = []
+
+with open("student_data.csv" , "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append(row)
+    
+def result(math, english, attendence):
+    average = (math + english) / 2
+
+    if average >= 50 and attendence >= 75:
+        return "Pass..."
+    else:
+        return "fail..."
+    
+print(students[0])
